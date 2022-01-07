0bd448942f079806e488f4e9b8977e4d028725f0
reko <reko@boom-cloud.com>
2022-01-07 17:38:28 +0800

    migrate to new embedding version

--- android/src/main/java/lyh/fluwx_worker/FluwxWorkerPlugin.java
+++ android/src/main/java/lyh/fluwx_worker/FluwxWorkerPlugin.java
@@ -1,21 +1,39 @@
 package lyh.fluwx_worker;
 
+import androidx.annotation.NonNull;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 
 /** FluwxWorkerPlugin */
-public class FluwxWorkerPlugin implements MethodCallHandler {
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "fluwx_worker");
-    APIHandler.setRegistrar(registrar);
-    ResponseHandler.setMethodChannel(channel);
+public class FluwxWorkerPlugin implements FlutterPlugin,MethodCallHandler {
 
+  private MethodChannel channel;
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "fluwx_worker");
+    APIHandler.setFlutterPluginBinding(flutterPluginBinding);
+    ResponseHandler.setMethodChannel(channel);
     channel.setMethodCallHandler(new FluwxWorkerPlugin());
   }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel.setMethodCallHandler(null);
+  }
+
+  /** Plugin registration. */
+//  public static void registerWith(Registrar registrar) {
+//    final MethodChannel channel = new MethodChannel(registrar.messenger(), "fluwx_worker");
+//    APIHandler.setRegistrar(registrar);
+//    ResponseHandler.setMethodChannel(channel);
+//
+//    channel.setMethodCallHandler(new FluwxWorkerPlugin());
+//  }
 
 
   @Override
